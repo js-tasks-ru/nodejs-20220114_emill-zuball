b96aa027dfef3e6c7f73b88d849243bbd13d973e
Emil Livingston <em@Emils-MacBook-Pro.local>
2022-01-25 16:03:08 +0300

    fix: add return

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -15,6 +15,7 @@ server.on('request', (req, res) => {
       if (pathname.includes('/')) {
         res.statusCode = 400;
         res.end('Nested directories are not supported');
+        return;
       }
 
       fs.readFile(filepath, (err, data) => {
