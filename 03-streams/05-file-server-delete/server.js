90298ff4a01cc135a6f4c80a843d22a3dd2d593c
Emil Livingston <em@Emils-MacBook-Pro.local>
2022-01-28 21:37:55 +0300

    03-task, 04 sub task: delete file

--- 03-streams/05-file-server-delete/server.js
+++ 03-streams/05-file-server-delete/server.js
@@ -1,6 +1,6 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -8,10 +8,47 @@ server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
+  if (pathname.includes('/')) {
+    res.statusCode = 400;
+    res.end('Nested directories are not supported');
+    return;
+  }
+
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'DELETE':
+
+      const readStream = fs.createReadStream(filepath);
+
+      readStream.on('error', (error) => {
+        if (error.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('File is not found');
+        } else {
+          res.statusCode = 500;
+          res.end('Server error');
+        }
+      });
+
+      readStream.on('data', () => {});
+
+      readStream.on('end', () => {
+        fs.unlink(filepath, (error) => {
+          if (error) {
+            res.statusCode = 500;
+            res.end();
+            return;
+          }
+
+          res.statusCode = 200;
+          res.end('File deleted successfully');
+        });
+      });
+
+      req.on('abort', () => {
+        readStream.destroy();
+      });
 
       break;
 
