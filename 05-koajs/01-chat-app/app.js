cf7f1971dff79ac8a13a2a03d7ca4b964e12cf4e
Emil Livingston <em@Emils-MacBook-Pro.local>
2022-01-30 20:09:34 +0300

    05-koajs/01-chat-app

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const Koa = require('koa');
 const app = new Koa();
+const EventEmitter = require('events');
 
 app.use(require('koa-static')(path.join(__dirname, 'public')));
 app.use(require('koa-bodyparser')());
@@ -8,10 +9,26 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+const chatEmitter = new EventEmitter();
+
 router.get('/subscribe', async (ctx, next) => {
+  await new Promise((resolve, reject) => {
+    chatEmitter.once('message', ({message}) => {
+      ctx.body = message;
+      resolve(ctx);
+    });
+  });
 });
 
 router.post('/publish', async (ctx, next) => {
+  if (ctx.request.body.message) {
+    chatEmitter.emit('message', ctx.request.body);
+    ctx.status = 200;
+    ctx.body = 'ok';
+  } else {
+    ctx.status = 400;
+    ctx.body = 'The message must be passed';
+  }
 });
 
 app.use(router.routes());
